35a07dc9c6d48d28fb6bf8db859fcdcd6fdd9e57
KryukovaLidiya <Lidakrukova1994@gmail.com>
2016-11-19 15:19:23 +0700

    Added SimQL impl

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -7,22 +7,34 @@ using Newtonsoft.Json.Linq;
 
 namespace SimQLTask
 {
-	class SimQLProgram
-	{
-		static void Main(string[] args)
-		{
-			var json = Console.In.ReadToEnd();
-			foreach (var result in ExecuteQueries(json))
-				Console.WriteLine(result);
-		}
+    class SimQLProgram
+    {
+        static void Main(string[] args)
+        {
+            var json = Console.In.ReadToEnd();
+            foreach (var result in ExecuteQueries(json))
+                Console.WriteLine(result);
+        }
 
-		public static IEnumerable<string> ExecuteQueries(string json)
-		{
-			var jObject = JObject.Parse(json);
-			var data = (JObject)jObject["data"];
-			var queries = jObject["queries"].ToObject<string[]>();
-			// TODO
-			return queries.Select(q => "TODO");
-		}
-	}
+        public static IEnumerable<string> ExecuteQueries(string json)
+        {
+            var jObject = JObject.Parse(json);
+            var data = (JObject)jObject["data"];
+            var queries = jObject["queries"].ToObject<string[]>();
+            return queries.Select(q =>
+            {
+                JObject tmp = data;
+                int i = 0;
+                foreach (string s in q.Split('.'))
+                {
+                    if (i < q.Split('.').Length - 1)
+                        tmp = (JObject)tmp[s];
+                    else
+                        return (q + " = " + (tmp[s] as JValue).ToString(CultureInfo.InvariantCulture));
+                    i++;
+                }
+                return "";
+            });
+        }
+    }
 }

aac12ad8c9267fa6ccb07f51de733966dd53b1fd
Anton Kiselev <straw.berry.pie@ya.ru>
2016-11-19 10:24:50 +0300

    Tests for Evaluator.

---
+++ Eval/Evaluator_Should.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace EvalTask
+{
+	[TestFixture]
+	public class Evaluator_Should
+	{
+		[Test]
+		public void AddTwoIntegers()
+		{
+			var input = "3 + 5";
+			var output = Evaluator.Evaluate(input);
+			Assert.AreEqual("8", output);
+
+		}
+
+		[Test]
+		public void MultiplyTwoIntegers()
+		{
+			var input = "3 * 5";
+			var output = Evaluator.Evaluate(input);
+			Assert.AreEqual("15", output);
+
+		}
+
+		[Test]
+		public void EvaluateAccordingToBraces()
+		{
+			var input = "(2 + 3) * 5";
+			var output = Evaluator.Evaluate(input);
+			Assert.AreEqual("25", output);
+
+		}
+	}
+}

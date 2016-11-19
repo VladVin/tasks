f5773e173de0eedf4e0185a150617704e5efb685
Vlad <vladt9@yandex.ru>
2016-11-19 15:54:25 +0300

    Fixed work with null products in JsonConverter

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -12,6 +12,8 @@ namespace JsonConversion
 		static void Main()
 		{
 			string json = Console.In.ReadToEnd();
+//		    string json = @"{""version"":""2"",""products"":{""431574347"":{""name"":""Avq7F"",""price"":202765200.0,""size"":[274716200.0,0.0,1.0],""count"":2147483647},""1644675389"":null,""0"":{""name"":""zR"",""price"":1569825105.0,""size"":[527150242.0,2147483647.0,2147483647.0]}}}";
+//		    string json = @"{""version"":""2"",""products"":{""1"":{""name"":""qrKI"",""size"":[0.0,0.0,1528659831.0],""count"":0},""0"":{""size"":[1893022325.0,0.0,805733179.0]},""671839910"":{""price"":2147483647.0,""size"":[1.0,1.0,960206352.0],""count"":2147483647}}}";
 //		    string json = @"{'version':'2'}";
 //		    string json = @"{'version':'2','products':{'1':{'name':'product-name','price':'12','count':100,'size':[12,130,131]}}}";
 //		    string json = @"{""version"":""2"",""constants"":{""p"":375570429.0,""BDT5b"":1895861230.0,""pk6rq0teL"":2147483647.0,""d801p5J6"":1994942430.0,""GuuguI"":0.0},""products"":{""0"":{""name"":""RQbl0WfVY"",""price"":""(-84.06633771214)*(p)"",""count"":881891749},""1"":{""name"":""kDaf0Z"",""price"":""(12.1639563293028)+(p)"",""count"":2147483647},""304322859"":{""name"":""KcOi9dvy"",""price"":""(pk6rq0teL)/(-37.0159578216336)"",""count"":1},""1408114756"":{""name"":""v4N"",""price"":""(-85.7739047081088)/(37.9361148634628)"",""count"":1},""1889271802"":{""name"":""wtXpL"",""price"":""(13.8310322136763)*(58.0251485845191)"",""count"":0},""687275581"":{""name"":""bp"",""price"":""(-92.3887056263111)-(d801p5J6)"",""count"":0},""1732466776"":{""name"":""IohrwvzQ"",""price"":""(d801p5J6)+(41.3040054688715)"",""count"":2147483647},""2147483647"":{""name"":""Abap"",""price"":""(-59.916888624391)-(-66.5829328664499)"",""count"":2147483647},""517841970"":{""name"":""KD9S4"",""price"":""(58.4912672445603)/(p)"",""count"":2147483647},""1362982700"":{""name"":""9pH"",""price"":""(-54.4264007147524)/(-45.6487527795363)"",""count"":1}}}";
@@ -38,7 +40,15 @@ namespace JsonConversion
                 prod.Add("id", int.Parse(jp.Name));
 
                 var children = child.Value<JToken>().Children();
+                
                 var ch = children.ToList()[0];
+
+                if (!ch.HasValues)
+                {
+                    prod.Add("products", null);
+                    newProducts.Add(prod);
+                    continue;
+                }
 
                 if (ch["name"] != null)
                 {
